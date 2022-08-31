9b77f78c2da71dac8ba209eaf01827a1d9ce2d19
Juan Pablo <juanp1310@outlook.com>
2022-08-31 08:49:12 -0300

    adicionando linear gradiente nos container menores

--- src/components/containerEnd/index.js
+++ src/components/containerEnd/index.js
@@ -6,7 +6,7 @@ import typeColors from "../../const";
 //components começam com letra maiuscula
 const ContainerEnd = (props) => {
     
-    return <article className='containerVistoUltimo' style={{backgroundColor:`${typeColors[props.tipo]}`}}> 
+    return <article className='containerVistoUltimo' style={{background: `linear-gradient(to top,${typeColors[props.tipo]}, ${props.tipo1 && typeColors[props.tipo1]})`}}> 
         <section className="rowContainerID"><section className="ContainerID"><span><b>{props.id}</b></span></section></section>
         <section className="rowImgPokemon">
                 <img className="imgPokemon" src={props.img} alt={props.nome} />
